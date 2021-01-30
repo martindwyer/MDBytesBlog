b1e96b18b6552a7c0671c5f908bed72eccc6ad1b
martindwyer <martin@mdbytes.com>
2021-01-30 05:59:26 -0600

    js updates

--- app/static/js/site.js
+++ app/static/js/site.js
@@ -2,54 +2,32 @@ console.log("JavaScript Ready");
 
 $page = document.querySelector("#page-indicator");
 
+document.querySelector("#home-link").classList.remove("active");
+document.querySelector("#about-link").classList.remove("active");
+document.querySelector("#contact-link").classList.remove("active");
+if (document.querySelector("#login-link")) {
+    document.querySelector("#login-link").classList.remove("active");
+}
+document.querySelector("#posts-link").classList.remove("active");
+
 switch ($page.innerHTML) {
     case "Home":
         document.querySelector("#home-link").classList.add("active");
-        document.querySelector("#about-link").classList.remove("active");
-        document.querySelector("#contact-link").classList.remove("active");
-        if (document.querySelector("#login-link")) {
-            document.querySelector("#login-link").classList.remove("active");
-        }
-        document.querySelector("#posts-link").classList.remove("active");
         break;
 
     case "About":
-        document.querySelector("#home-link").classList.remove("active");
         document.querySelector("#about-link").classList.add("active");
-        document.querySelector("#contact-link").classList.remove("active");
-        if (document.querySelector("#login-link")) {
-            document.querySelector("#login-link").classList.remove("active");
-        }
-        document.querySelector("#posts-link").classList.remove("active");
         break;
 
     case "Contact":
-        document.querySelector("#home-link").classList.remove("active");
-        document.querySelector("#about-link").classList.remove("active");
         document.querySelector("#contact-link").classList.add("active");
-        if (document.querySelector("#login-link")) {
-            document.querySelector("#login-link").classList.remove("active");
-        }
-        document.querySelector("#posts-link").classList.remove("active");
         break;
 
     case "Login":
-        document.querySelector("#home-link").classList.remove("active");
-        document.querySelector("#about-link").classList.remove("active");
-        document.querySelector("#contact-link").classList.remove("active");
-        if (document.querySelector("#login-link")) {
-            document.querySelector("#login-link").classList.add("active");
-        }
-        document.querySelector("#posts-link").classList.remove("active");
+        document.querySelector("#login-link").classList.add("active");
         break;
 
     case "Posts":
-        document.querySelector("#home-link").classList.remove("active");
-        document.querySelector("#about-link").classList.remove("active");
-        document.querySelector("#contact-link").classList.remove("active");
-        if (document.querySelector("#login-link")) {
-            document.querySelector("#login-link").classList.remove("active");
-        }
         document.querySelector("#posts-link").classList.add("active");
         break;
 
